673744a76eb98a1089bd10533662ffed0a6d2363
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-07 08:50:36 +0900

    feat: todolist css

---
+++ 04_todoList/src/TodoListApp.jsx
@@ -0,0 +1,23 @@
+import "./todolist.css"
+
+function TodoListApp() {
+    return (
+        <div class="todo">
+            <h1 class="todo__title">Todo List App</h1>
+            <form class="todo__form">
+                <input type="text" placeholder="할 일 목록을 입력하세요." className="todo__input"/>
+                <button type="submit" className="todo__button todo__button--add">추가</button>
+            </form>
+            <ul className="todo__list">
+                <li className="todo__item todo__item--completed">
+                    <input type="checkbox" className="todo__check" name="" id="" />
+                    <label htmlFor="" className="todo__label"> 옷싸기</label>
+                    <button className="todo__button todo__button--edit">✏️</button>
+                    <button className="todo__button todo__button--delete">❌</button>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default TodoListApp
---
+++ 04_todoList/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './reset.css'
+import TodoListApp from './TodoListApp'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <TodoListApp />
+  </StrictMode>,
+)
