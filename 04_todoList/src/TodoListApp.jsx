dff1b680a10858d9539f5b2c70fbdce1faf9c466
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-07 09:34:54 +0900

    TodoListApp HTML todo__item--empty

--- 04_todoList/src/TodoListApp.jsx
+++ 04_todoList/src/TodoListApp.jsx
@@ -9,7 +9,10 @@ function TodoListApp() {
                 <button type="submit" className="todo__button todo__button--add">추가</button>
             </form>
             <ul className="todo__list">
-                <li className="todo__item todo__item--completed">
+                <li className="todo__item todo__item--empty">
+                    <p>할 일 없음</p>
+                </li>  
+                <li className="todo__item todo__item--complete">
                     <input type="checkbox" className="todo__check" name="" id="chk-1" />
                     <label htmlFor="chk-1" className="todo__label"> 옷싸기</label>
                     <button className="todo__button todo__button--edit">✏️</button>
