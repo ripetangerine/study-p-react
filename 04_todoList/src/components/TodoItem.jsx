833479578371ff2acd00a969517b19049838f2c1
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-21 09:00:07 +0900

    feat: TodoItem className 조건부 렌더링

--- 04_todoList/src/components/TodoItem.jsx
+++ 04_todoList/src/components/TodoItem.jsx
@@ -1,13 +1,14 @@
 import CheckBox from "./CheckBox.jsx"
 import Button from "./Button.jsx"
 
-export default function TodoItem({todos}){
+export default function TodoItem({todo}){
     return(
-    <li>
-        <CheckBox type="checkbox" className="todo__check" id="1"/>
-        {todos}
-        <Button className="todo__button todo__button--edit">수정해</Button>
-        <Button className="todo__button todo__button--edit">삭제해</Button>
+        // todo.isCompleted가 true면 " todo__item--complete" 넣긔, false ""
+    <li className={`todo__item ${todo.isCompleted ? "todo__item--complete":""}`}>
+        <CheckBox type="checkbox" className="todo__check" id={todo.id}>{todo.text}</CheckBox>
+        
+        <Button className="todo__button todo__button--edit">🔮</Button>
+        <Button className="todo__button todo__button--delete">🗑️</Button>
     </li>
     )
 }
